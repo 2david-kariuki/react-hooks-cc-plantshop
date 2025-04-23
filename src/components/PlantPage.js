3b52389ec37e477220712859b13eaaa8589ddd11
David <david48kariuki@gmail.com>
2025-04-24 02:50:05 +0300

    complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) =>
+        setPlants(data.map((plant) => ({ ...plant, inStock: true })))
+      );
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    const plantToSend = {
+      name: newPlant.name,
+      image: newPlant.image,
+      price: newPlant.price.toString(),
+    };
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(plantToSend),
+    })
+      .then((res) => res.json())
+      .then((data) => setPlants([...plants, { ...data, inStock: true }]));
+  };
+
+  const handleToggleStock = (id) => {
+    setPlants(
+      plants.map((plant) =>
+        plant.id === id ? { ...plant, inStock: !plant.inStock } : plant
+      )
+    );
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} onToggleStock={handleToggleStock} />
     </main>
   );
 }
